debff48607fbbc9b5fab3614b5ce76a3ede9518c
coderlisan <enginerlisan@gmail.com>
2022-06-16 10:02:19 +0600

    new version

---
+++ footer.php
@@ -0,0 +1,70 @@
+  
+  <footer class="footer-bg m-top0">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-12 m-bottom2">
+          <div class="call-to-action clearfix"><span class="fa fa-envelope-o font30 left"></span>
+            <div class="feature-icon-col font-white left-padd9 m-top1">
+              <h2 class="font20 font-white m-bottom1">Email</h2>
+              <p><a href="mailto:info@example.com">info@example.com</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-12 m-bottom2">
+          <div class="call-to-action clearfix"><span class="fa fa-phone font30 left"></span>
+            <div class="feature-icon-col font-white left-padd9 m-top1">
+              <h2 class="font20 font-white m-bottom1">Phone</h2>
+              <p>+1 123 4567 890</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-12 m-bottom2">
+          <div class="call-to-action clearfix"><span class="fa fa-map-marker font30 left"></span>
+            <div class="feature-icon-col font-white left-padd9 m-top1">
+              <h2 class="font-white font20 m-bottom1">Location</h2>
+              <p>Road #152, Chorley, NJ, USA</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <div class="copyrights">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-md-12">
+          <ul class="social-icons">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fa fa-flickr"></i></a></li>
+            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+            <li><a href="#"><i class="fa fa-vimeo-square"></i></a></li>
+            <li><a href="#"><i class="fa fa-rss"></i></a></li>
+          </ul>
+        </div>
+        <div class="col-md-12 m-top4"> Copyright &copy; 2016 yourdomian. All Rights Reserved. </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<a href="#" class="scrollup"></a>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/masterslider/jquery.easing.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/masterslider/masterslider.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/customjs1.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/cubeportf/jquery-latest.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/cubeportf/jquery.cubeportfolio.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/cubeportf/main4.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/form/jquery-ui.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/form/jquery.form.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/form/jquery.validate.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/mainmenu/sticky.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/scrolltotop/totop.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/animations/animations.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/customjs2.js"></script>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+add_action('after_setup_theme', 'my_theme_setup');
+function my_theme_setup() {
+  add_theme_support('menus');
+  add_theme_support('widgets');
+  add_theme_support('custom-header');
+  add_theme_support('custom-background');
+  add_theme_support('post-formats', ['video', 'audio', 'gallery']);
+  add_theme_support('post-thumbnails');
+  add_theme_support('custom-logo');
+  add_theme_support('title-tag');
+  register_nav_menus(['main_menu' => 'Main Menu', 'footer_1' => 'Foooter 1', 'footer_2' => 'Foooter 2']);
+
+  register_post_type('clients', [
+    'public' => true,
+    'labels' => ['name' => 'Clients','all_items' => 'All Clients','add_new' => 'Add new Clients','add_new_item' => 'Add new Clients','featured_image' => 'Clients Image','set_featured_image' => 'Set Clients Image'],
+    'supports' => ['title', 'thumbnail', 'editor'],
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-format-aside',
+  ]);
+}
+
+add_action('wp_enqueue_scripts', 'css_connect');
+function css_connect() {
+  wp_enqueue_style('C01', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
+  wp_enqueue_style('C02', get_template_directory_uri() . '/assets/css/style.css');
+  wp_enqueue_style('C03', get_template_directory_uri() . '/assets/css/reset.css');
+  wp_enqueue_style('C04', get_template_directory_uri() . '/assets/css/font-awesome/css/font-awesome.min.css');
+  wp_enqueue_style('C05', get_template_directory_uri() . '/assets/css/Icons-Webfont/simple-line-icons.css');
+  wp_enqueue_style('C06', get_template_directory_uri() . '/assets/css/et-line-font/et-line-font.css');
+  wp_enqueue_style('C07', get_template_directory_uri() . '/assets/css/responsive-leyouts.css');
+  wp_enqueue_style('C08', get_template_directory_uri() . '/assets/js/mainmenu/menu.css');
+  wp_enqueue_style('C09', get_template_directory_uri() . '/assets/js/masterslider/style/masterslider.css');
+  wp_enqueue_style('C10', get_template_directory_uri() . '/assets/js/masterslider/skins/default/style.css');
+  wp_enqueue_style('C11', get_template_directory_uri() . '/assets/js/masterslider/style/style.css');
+  wp_enqueue_style('C12', get_template_directory_uri() . '/assets/js/cubeportf/css/cubeportfolio.min.css');
+  wp_enqueue_style('C13', get_template_directory_uri() . '/assets/js/form/css/sky-forms.css');
+  wp_enqueue_style('C14', get_template_directory_uri() . '/assets/js/animations/css/animations.min.css');
+  wp_enqueue_style('C00', get_stylesheet_uri());
+  wp_enqueue_script('jquery');
+}
